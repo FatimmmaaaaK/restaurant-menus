6cbf853c53f821a5d2a0944428ded5f51125f503
Fatoumata Kaba <fkabanoun@gmail.com>
2023-03-03 11:43:49 -0500

    all tests passing, Models defined.

--- db.js
+++ db.js
@@ -2,6 +2,10 @@ const path = require('path');
 const { Sequelize } = require('sequelize');
 
 // TODO - connect to db via sequelize
+const sequelize = new Sequelize({
+    dialect:'sqlite',
+    storage: '/Users/fatoumata.kaba/restaurant-menus/db.sqlite'
+})
 
 module.exports = {
     sequelize
--- index.test.js
+++ index.test.js
@@ -18,26 +18,68 @@ describe('Restaurant and Menu Models', () => {
 
     test('can create a Restaurant', async () => {
         // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        const newRestaurant = await Restaurant.create({
+            name: 'The Sushi Bar',
+            location: 'Hackensack',
+            cuisine: 'Japanese'
+
+        });
+
+        expect(newRestaurant.name).toEqual('The Sushi Bar'),
+        expect(newRestaurant.location).toEqual("Hackensack"),
+        expect(newRestaurant.cuisine).toEqual('Japanese')
+
     });
 
     test('can create a Menu', async () => {
         // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        const newMenu = await Menu.create({
+            title: 'Be our Guest'
+        });
+        
+        expect(newMenu.title).toEqual('Be our Guest')
+ 
     });
 
     test('can find Restaurants', async () => {
         // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        const newRestaurant = await Restaurant.create({
+            name: 'The Sushi Bar',
+            location: 'Hackensack',
+            cuisine: 'Japanese'
+
+        });
+        const allRestaurants = await Restaurant.findByPk(1);
+
+        expect(allRestaurants.name).toEqual('The Sushi Bar')
     });
 
     test('can find Menus', async () => {
         // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        const newMenu = await Menu.create({
+            title: 'Be our Guest'
+        });
+
+        const firstMenu = await Menu.findByPk(1);
+        expect(firstMenu.title).toEqual('Be our Guest');
     });
 
     test('can delete Restaurants', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        // TODO - write tests
+        const newRestaurant = await Restaurant.create({
+            name: 'The Diner',
+            location: 'Main street',
+            cuisine:'American'
+        });
+
+        const deletedRestaurant = await Restaurant.destroy({
+            where:{
+                id:1
+            }
+        });
+        const foundRestaurant = await Restaurant.findByPk(1);
+        console.log(foundRestaurant);
+
+        expect(foundRestaurant).toBe(null)
     });
-})
+}) 
